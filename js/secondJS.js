ac9a55337bf4700e826a3696c6d8757708b5a7ef
justno1 <justno1@hotmail.com.tw>
2014-06-15 22:09:16 +0800

    ABGT add

--- js/secondJS.js
+++ js/secondJS.js
@@ -65,7 +65,7 @@
             {
             
             var objectNews = news[i];
-            var newsIDinParse = objectNews.get("objectId");
+            var newsIDinParse = objectNews.get("objectid");
             var newsTitle = objectNews.get("title");
             var newsPicture = objectNews.get("picture");
             var newsText = objectNews.get("text");
