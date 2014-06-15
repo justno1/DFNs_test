65c9d962a1c112fe3fdb7f0764d7e3e04dac1a3a
justno1 <justno1@hotmail.com.tw>
2014-06-15 16:50:36 +0800

    ABGT add

--- js/secondJS.js
+++ js/secondJS.js
@@ -82,6 +82,18 @@
     //if not log in >> pop out
     $('#logInButton').on('click',function(){
 
+        //LOAD FACEBOOK SDK ASYNC
+        (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js"; 
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
         window.fbAsyncInit = function () {
 
           alert("FB init!!");
@@ -91,18 +103,6 @@
             xfbml: true,
             version: 'v2.0'
           });
-
-          //LOAD FACEBOOK SDK ASYNC
-          (function (d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) {
-              return;
-          }
-          js = d.createElement(s);
-          js.id = id;
-          js.src = "//connect.facebook.net/en_US/sdk.js"; 
-          fjs.parentNode.insertBefore(js, fjs);
-          }(document, 'script', 'facebook-jssdk'));
 
           FB.getLoginStatus(function(response) {
               if (response.status === 'connected') {
