fc0140dc163e8ec163c6f693819dff4232bab555
justno1 <justno1@hotmail.com.tw>
2014-06-18 16:58:26 +0800

    ABGT add

--- js/thirdtestJS.js
+++ js/thirdtestJS.js
@@ -1,9 +1,9 @@
 Parse.initialize("LXu553qsB1idva0RxKbksnYdbwn54XQE1JxPX4rJ", "6Bh7L4V3kTKPoINzY72A1AYcMWdT1jnJuhHtnPGD");
 
 $(window).bind("load",function(){
-//    var userFBIdForParse = window.id;
+    var userFBIdForParse = window.id;
       alert(window.id);
-});
+
 
 var strurl = location.search;
 var ParaVal;
@@ -285,4 +285,4 @@ var queryqueryquery = new Parse.Query(DataThree);
 
     }
   });
-
+});
