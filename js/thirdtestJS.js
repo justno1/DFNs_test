d0a5076f4b250066e923ee7f1a3b30519c04a5ff
justno1 <justno1@hotmail.com.tw>
2014-06-18 16:08:04 +0800

    ABGT add

--- js/thirdtestJS.js
+++ js/thirdtestJS.js
@@ -1,17 +1,17 @@
 Parse.initialize("LXu553qsB1idva0RxKbksnYdbwn54XQE1JxPX4rJ", "6Bh7L4V3kTKPoINzY72A1AYcMWdT1jnJuhHtnPGD");
 
-var userIdForParse = "";
-
-
-window.fbAsyncInit = function () {
-FB.getLoginStatus(function(response) {
-  if (response.status === 'connected') {
-    FB.api('/me', function (response) {
-      userIdForParse = response.id;
-      });     
-  }
+$( document ).ready(function() {
+  FB.getLoginStatus(function(response) {
+    if (response.status === 'connected') {
+      FB.api('/me', function (response) {
+        userIdForParse = response.id;
+        });     
+    }
   };
-};
+});
+
+
+var userIdForParse = "";
 
 var strurl = location.search;
 var ParaVal;
