398464b0a14be3087a7e060cc579598fa4a0a99e
justno1 <justno1@hotmail.com.tw>
2014-06-18 15:13:33 +0800

    ABGT add

--- js/fb.js
+++ js/fb.js
@@ -22,7 +22,7 @@ FB.getLoginStatus(function(response) {
         FB.api('/me', {fields: 'last_name'}, function(response) {
             console.log(response);
         });
-        document.getElementById("getLoginStatus").style.display="none";
+        document.getElementById("login").style.display="none";
         document.getElementById("logout").style.display="block";
         
     
@@ -77,7 +77,10 @@ FB.getLoginStatus(function(response) {
 
 $("#logout").click(function () {
   alert("In Logout");
+    document.getElementById("login").style.display="block";
+    document.getElementById("logout").style.display="none";
     FB.logout();
+    window.location.href = "index.html"
 });
 
 
